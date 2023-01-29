f7c7a4c4d5969a6c5e8e8d199971348dfba1bfa2
Dawson Israel <dawfre@amazon.com>
2023-01-29 20:57:01 -0300

    Return Json instead of plain text

--- src/controller/account_controller.rs
+++ src/controller/account_controller.rs
@@ -1,6 +1,8 @@
-use lambda_web::actix_web::{get, Responder};
+use lambda_web::actix_web::{get, http::header::ContentType, HttpResponse};
 
 #[get("/accounts")]
-pub async fn list_accounts() -> impl Responder {
-    format!("TODO")
+pub async fn list_accounts() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body("{}")
 }
