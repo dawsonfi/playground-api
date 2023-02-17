08b3a90e7b405d5209c3a59eb93547c7f82443ee
Dawson Israel <dawfre@amazon.com>
2023-02-17 14:17:29 -0300

    Fix Integ Tests

--- tests/accounts_integration_test.rs
+++ tests/accounts_integration_test.rs
@@ -4,6 +4,8 @@ mod agent;
 #[cfg(test)]
 mod tests {
     use crate::agent::{build_playground_api_agent, PlaygroundApiRequest};
+    use playground_api::model::account::Account;
+    use serde_json::from_str;
 
     #[tokio::test]
     async fn test_list_accounts() {
@@ -18,7 +20,8 @@ mod tests {
             .await
             .unwrap();
 
+        let accounts: Vec<Account> = from_str(&result.payload).unwrap();
         assert_eq!(result.status, 200);
-        assert_eq!(result.payload, "{}".to_string());
+        assert!(accounts.len() > 0);
     }
 }
