d77ff426e6d35629b108ebcea111fc41e61aea7b
Dawson Israel <dawfre@amazon.com>
2023-02-21 12:03:43 -0300

    Add Log Configuration and Introduce Data on actix

--- src/controller/account_controller.rs
+++ src/controller/account_controller.rs
@@ -1,12 +1,10 @@
-use crate::repository::{account_repository::AccountRepository, ConfigProvider};
+use crate::repository::account_repository::AccountRepository;
+use actix_web::web::Data;
 use lambda_web::actix_web::{get, http::header::ContentType, HttpResponse};
 use serde_json::to_string;
 
 #[get("/accounts")]
-pub async fn list_accounts() -> HttpResponse {
-    let sdk_config = ConfigProvider::default().provide().await;
-    let account_repository = AccountRepository::new(&sdk_config);
-
+pub async fn list_accounts(account_repository: Data<AccountRepository>) -> HttpResponse {
     let accounts = account_repository.list_accounts(None, None).await.unwrap();
 
     HttpResponse::Ok()
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use actix_web::web::Data;
+use env_logger::{init_from_env, Env};
 use lambda_web::actix_web::{self, App, HttpServer};
 use lambda_web::{is_running_on_lambda, run_actix_on_lambda, LambdaError};
 use playground_api::controller::account_controller::list_accounts;
+use playground_api::repository::account_repository::AccountRepository;
+use playground_api::repository::ConfigProvider;
 
 #[actix_web::main]
 async fn main() -> Result<(), LambdaError> {
-    let factory = move || App::new().service(list_accounts);
+    configure_log();
+
+    let config_provider = ConfigProvider::default().provide().await;
+    let factory = move || {
+        App::new()
+            .service(list_accounts)
+            .app_data(Data::new(AccountRepository::new(&config_provider)))
+    };
 
     if is_running_on_lambda() {
         // Run on AWS Lambda
@@ -17,4 +28,9 @@ async fn main() -> Result<(), LambdaError> {
             .await?;
     }
     Ok(())
+}
+
+fn configure_log() {
+    let env = Env::default().filter_or("MY_LOG_LEVEL", "info");
+    init_from_env(env);
 }
