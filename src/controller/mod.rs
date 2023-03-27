577e07d38de6cd81e9c5130aac0e5089cc692962
Dawson Israel <dawfre@amazon.com>
2023-03-27 11:48:27 -0300

    Add Unit tests to controller Exception Response Transform

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -19,3 +19,50 @@ impl ResponseError for PermanentError {
             )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::body::MessageBody;
+    use actix_web::http::StatusCode;
+    use std::error::Error;
+    use std::fmt::{Display, Formatter};
+
+    #[test]
+    fn should_return_internal_server_error() {
+        let error = PermanentError {
+            source: Box::new(TestError {}),
+            message: Some("test".to_string()),
+        };
+
+        let error_response = error.error_response();
+
+        assert_eq!(error_response.status(), StatusCode::INTERNAL_SERVER_ERROR);
+
+        assert_eq!(
+            error_response
+                .headers()
+                .get("Content-Type")
+                .unwrap()
+                .to_str()
+                .unwrap(),
+            "application/json"
+        );
+
+        assert_eq!(
+            format!("{:?}", error_response.into_body().try_into_bytes().unwrap()),
+            "b\"{\\\"cause\\\":\\\"error: test\\\"}\""
+        );
+    }
+
+    #[derive(Debug)]
+    struct TestError {}
+
+    impl Display for TestError {
+        fn fmt(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
+            write!(formatter, "error")
+        }
+    }
+
+    impl Error for TestError {}
+}
